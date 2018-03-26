057b0a6536aae9fdd1a1c75447bf73933bb373ea
onokatio <onokatio@gmail.com>
2018-03-26 19:26:46 +0900

    add haml config

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+				Bkwld\LaravelHaml\ServiceProvider::class,
 
         /*
          * Application Service Providers...
